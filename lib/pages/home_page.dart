c6c91c479c002a03194d4413058cfbf2377dc840
DESKTOP-1GGTS4B\a <dobariyas.cool@gmail.com>
2022-07-23 10:06:11 +0530

    day 2 commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,8 @@ class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Coding";
 
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     
